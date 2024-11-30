da083d33fabd6e819517395daecba13ccca4d09e
pradeep1399 <pradeepverma1399@gmail.com>
2024-11-30 16:59:22 +0530

    added user curd operation

--- rbac/src/main/java/com/spring/rbac/controller/UsersController.java
+++ rbac/src/main/java/com/spring/rbac/controller/UsersController.java
@@ -1,12 +1,52 @@
 package com.spring.rbac.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.rbac.model.Users;
+import com.spring.rbac.service.UsersService;
+import org.apache.coyote.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 public class UsersController {
 
+    @Autowired
+    private UsersService usersService;
+
 
     @GetMapping("users")
+    public ResponseEntity<List<Users>> fetchUsers(){
+        List<Users> users = usersService.getUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/users/{userName}")
+    public ResponseEntity<Users> fetchUserByUserName(@PathVariable String userName){
+        Users user = usersService.getUsersByUserName(userName);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<String> createUser(@RequestBody Users user){
+        usersService.saveUser(user);
+        return  ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<String> updateuser(@RequestBody Users user){
+        usersService.saveUser(user);
+        return  ResponseEntity.status(HttpStatus.CREATED).body("User updated successfully");
+    }
+
+    @DeleteMapping("/users/{userName}")
+    public ResponseEntity<String> deleteUser(@PathVariable String userName) throws Exception {
+        usersService.deleteUserByUserName(userName);
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfullty");
+    }
+
 }
--- rbac/src/main/java/com/spring/rbac/repository/UsersRepository.java
+++ rbac/src/main/java/com/spring/rbac/repository/UsersRepository.java
@@ -3,6 +3,10 @@ package com.spring.rbac.repository;
 import com.spring.rbac.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users, String> {
+import java.util.List;
 
+public interface UsersRepository extends JpaRepository<Users, String> {
+    List<Users> findAll();
+    Users findByUserName(String userName);
+    void deleteByUserName(String userName);
 }
