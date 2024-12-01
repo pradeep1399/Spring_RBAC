37c48eb9c5834054919f0c976c140bee770f809b
pradeep1399 <pradeepverma1399@gmail.com>
2024-12-01 17:19:07 +0530

    updated PermissionRepository

--- rbac/src/main/java/com/spring/rbac/repository/PermissionRepository.java
+++ rbac/src/main/java/com/spring/rbac/repository/PermissionRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, String> {
-    Object findByName(String read);
 }
